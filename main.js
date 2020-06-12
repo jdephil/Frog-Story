20d6ffa76bcec39c7d86cc9069531af983747f42
jdephil <jennifer.dephillips@gmail.com>
2020-06-12 14:22:59 -0400

    cleaned up html & css, starter js

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     /*---DOM refs---*/
-    let frog = document.getElementById
+    let frog = document.getElementById('frog-img')
+    let flies = document.getElementsByClassName('flyImgs')
+    let pond = document.getElementById('pond-img')
+    let btn = document.querySelector('button')
+   
 
+    /*---Game logic---*/
+    let gameOver = false
+    let introText = []
 
+    /*---Event Listeners---*/
+    frog.addEventListener('click', init)
+    flies.addEventListener('click', flySwarm)
+    pond.addEventListener('click', drinkWater)
+    btn.addEventListener('click', reset)
+    document.addEventListener('keydown', leaveTown)
 
+    let init = () => {
+        console.log('init check')
+        gameOver = false;
+        let flyBuzz = document.createElement('audio')
+        flyBuzz.innerHTML = "<source src='fly-buzz.mp3' type='audio/mpeg>"
+        document.querySelector('main').append(flyBuzz)
+        let introP = document.createElement('p')
+        
+        //add code to move frog
+        //add code to set gameboard back to normal?
+
+    }
 
 })
